05249d946b9cf37bbda7a83b2ba50d81809f75ab
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-24 13:16:27 -0300

    Update exe03_li1_casa.c

--- Primeiros contatos/Lista 1 - Casa/exe03_li1_casa.c
+++ Primeiros contatos/Lista 1 - Casa/exe03_li1_casa.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-	float AP, LP, AA, LA, AN;
+	float ap, lp, aa, la, an;
 	printf("Insira a altura da parede: ");
-	scanf("%f", &AP);
+	scanf("%f", &ap);
 	printf("Insira a largura da parede: ");
-	scanf("%f", &LP);
+	scanf("%f", &lp);
 	printf("Insira a altura do azulejo: ");
-	scanf("%f", &AA);
+	scanf("%f", &aa);
 	printf("Insira a largura do azulejo: ");
-	scanf("%f", &LA);
-	AN=(AP*LP)/(AA*LA);
-	printf("O numero de azulejos necessarios e igual a: %f\n", AN);
+	scanf("%f", &la);
+	an=(ap*lp)/(aa*la);
+	printf("O numero de azulejos necessarios e igual a: %f\n", an);
 	getch();
 	return 0;
 }
