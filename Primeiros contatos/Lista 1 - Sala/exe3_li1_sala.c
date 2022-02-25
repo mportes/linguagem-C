6d1c0b99cd0795f0e22386d90a46f313cec5d2bc
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-25 09:44:49 -0300

    Update exe3_li1_sala.c

--- Primeiros contatos/Lista 1 - Sala/exe3_li1_sala.c
+++ Primeiros contatos/Lista 1 - Sala/exe3_li1_sala.c
@@ -4,13 +4,13 @@
 
 int main()
 {
-	float V, R, A;
+	float v, r, a;
 	printf("Insira o valor do raio do fundo da lata: ");
-	scanf("%f", &R);
+	scanf("%f", &r);
 	printf("Insira o valor da altura da lata: ");
-	scanf("%f", &A);
-	V=M_PI*R*R*A;
-	printf("O volume da lata de oleo e igual a: %f\n", V);
+	scanf("%f", &a);
+	v=M_PI*r*r*a;
+	printf("O volume da lata de oleo e igual a: %f\n", v);
 	getch();
 	return 0;
 }
