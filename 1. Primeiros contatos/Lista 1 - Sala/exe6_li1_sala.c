b0213464ac9da4c894fd2bd008fc3fc91eb1dfc5
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-25 11:24:12 -0300

    Add files via upload

---
+++ 1. Primeiros contatos/Lista 1 - Sala/exe6_li1_sala.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	float p, v, taxa, tempo;
+	printf("Insira o valor normal da prestacao: ");
+	scanf("%f", &v);
+	printf("Insira o valor da taxa: ");
+	scanf("%f", &taxa);
+	printf("Insira o tempo: ");
+	scanf("%f", &tempo);
+	p=v+(v*(taxa/100)*tempo);
+	printf("O valor da prestacao em atraso e igual a %f\n", p);
+	getch();
+	return 0;
+}
