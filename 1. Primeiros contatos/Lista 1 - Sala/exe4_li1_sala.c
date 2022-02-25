ceff4cacc99ab4f5709e72244d088d10711ab49e
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-25 11:24:09 -0300

    Add files via upload

---
+++ 1. Primeiros contatos/Lista 1 - Sala/exe4_li1_sala.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	int a, b, c;
+	printf("Insira o primeiro numero: ");
+	scanf("%i", &a);
+	printf("Insira o segundo numero: ");
+	scanf("%i", &b);
+	c=a;
+	a=b;
+	b=c;
+	printf("Os valores trocados resultaram em: %i %i\n", a, b);
+	getch();
+	return 0;
+}
