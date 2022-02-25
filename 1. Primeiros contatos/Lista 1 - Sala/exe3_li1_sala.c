50e21f52cf27070c62dcf32c30ac2f47caf8fe32
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-25 11:24:07 -0300

    Add files via upload

---
+++ 1. Primeiros contatos/Lista 1 - Sala/exe3_li1_sala.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main() {
+	float v, r, a;
+	printf("Insira o valor do raio do fundo da lata: ");
+	scanf("%f", &r);
+	printf("Insira o valor da altura da lata: ");
+	scanf("%f", &a);
+	v=M_PI*r*r*a;
+	printf("O volume da lata de oleo e igual a: %f\n", v);
+	getch();
+	return 0;
+}
