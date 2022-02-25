a1dd54f9fc3860412086c2aefcd6b0a71e291ff3
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-25 09:44:16 -0300

    Update exe2_li1_sala.c

--- Primeiros contatos/Lista 1 - Sala/exe2_li1_sala.c
+++ Primeiros contatos/Lista 1 - Sala/exe2_li1_sala.c
@@ -3,11 +3,11 @@
 
 int main()
 {
-	float C, F;
+	float c, f;
 	printf("Digite a temperatura em Fahrenheit: ");
-	scanf("%f", &F);
-	C=(((F-32)*5)/9);
-	printf("A temperatura em Celsius e: %f\n", C);
+	scanf("%f", &f);
+	c=(((f-32)*5)/9);
+	printf("A temperatura em Celsius e: %f\n", c);
 	getch();
 	return 0;
 }
