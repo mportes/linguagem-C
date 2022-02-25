ac386c6aa45274e791d27ec1e8ef0c360191d1fe
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-02-25 11:20:51 -0300

    Add files via upload

---
+++ 1. Primeiros contatos/Lista 1 - Casa/exe02_li1_casa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	float r, d;
+	printf("Digite o valor em dolares: ");
+	scanf("%f", &d);
+	r=d*2.4;
+	printf("O valor de %f dolares convertido para reais e igual a %f reais\n", d, r);
+	getch();
+	return 0;
+}
