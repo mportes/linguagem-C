d4cf52c4359d6af7fe26e0707544eb0a575cf2a7
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-03-02 15:12:12 -0300

    Add files via upload

---
+++ 4. Vetores/Lista 4 - Sala/exe8_li4_sala.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	int m;
+	
+	printf("Digite o número do mês: ");
+	scanf("%d", &m);
+	printf("\n\n");
+	
+	switch(m) {
+		case 1: printf("Janeiro!"); break;
+		case 2: printf("Fevereiro!"); break;
+		case 3: printf("Março!"); break;
+		case 4: printf("Abril!"); break;
+		case 5: printf("Maio!"); break;
+		case 6: printf("Junho!"); break;
+		case 7: printf("Julho!"); break;
+		case 8: printf("Agosto!"); break;
+		case 9: printf("Setembro!"); break;
+		case 10: printf("Outubro!"); break;
+		case 11: printf("Novembro!"); break;
+		case 12: printf("Dezembro!"); break;
+		default: printf("Número inválido!");
+	}
+	
+	getch();
+	return 0;
+}
