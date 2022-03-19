e9c032c52fe9770266f2deef1d2c2abcaf73aa89
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-03-19 16:21:06 -0300

    Add files via upload

---
+++ 8. Funções/exe3a_li8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+int soma(int *a) {
+	int s=0;
+	
+	for(int i=*a; i>0; --i) s=s+i;
+	
+	printf("Resultado da soma: %d\n\n", s);
+}
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	int s;
+	int a;
+	
+	printf("Digite um número para somar com todos os seus antecessores>0: ");
+	scanf("%d", &a);
+	
+	if(a==0){
+		getch();
+		return 0;
+	}
+	
+	soma(&a);
+	
+	getch();
+	return 0;
+}
