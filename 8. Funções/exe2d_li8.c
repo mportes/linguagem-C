fe4e4e0195e0aa3902a88473657739e1ef0f0c5b
Matheus Portes <65133000+mportes@users.noreply.github.com>
2022-03-19 16:21:00 -0300

    Add files via upload

---
+++ 8. Funções/exe2d_li8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+
+
+int potencia(float a, float b) {
+	float p;
+	p=pow(a, b);
+	printf("Resultado da potência: %.2f", p);
+}
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	float a,b;
+		
+	printf("Digite o número a ser exponenciado: ");
+	scanf("%f", &a);
+	
+	printf("Digite a potência: ");
+	scanf("%f", &b);
+	
+	potencia(a, b);
+	
+	getch();
+	return 0;
+}
